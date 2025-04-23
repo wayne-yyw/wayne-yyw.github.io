11e46afdceaa52edf727c5412b62d7c72099c17f
yyw <15927549269@163.com>
2025-04-23 10:40:41 +0800

    1

---
+++ src/components/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">用于测试部署网页！</div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped>
+.hello {
+  font-size: 20px;
+  font-weight: bold;
+}
+</style>
